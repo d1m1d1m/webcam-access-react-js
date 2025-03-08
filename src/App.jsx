dd874df69790f3d623e0adcd051ffcd3428eae5c
D1m1tr1 <d1m1tr1.d3l3tang@gmail.com>
2025-03-09 00:28:51 +0100

    add function as camera handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { useRef } from "react";
+
 const App = () => {
+    const videoRef = useRef(null);
+
+    const openCamera = async () => {
+        const constraints = { audio: false, video: {
+            facingMode: "environment"
+        } };
+        const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+        if(mediaStream) {
+            const videoEl = videoRef.current;
+            videoEl.srcObject = mediaStream;
+            videoEl.onloadedmetadata = function (e) {
+                videoEl.play();
+            };
+        }
+    }
 
     return(
         <>
             <h1>Hello World</h1>
+            <button onClick={openCamera}>Open Camera On Mobile</button>
+
+            <video width={500} height={500} ref={videoRef}></video>
         </>
     );
 };
